68c4204ab6e74826014d0fa06fb07c97b724e2fc
Oliver Kriska <quatermain32@gmail.com>
2014-09-23 13:30:32 +0200

    Samostatne zadanie 4: Zakladne zobrazovanie clankov

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918092656) do
+ActiveRecord::Schema.define(version: 20140923105211) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
     t.text     "content"
     t.string   "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
