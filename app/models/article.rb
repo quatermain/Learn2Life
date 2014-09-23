6cc4be34d060f615961b6f6a50cae840acc7dbb3
Oliver Kriska <quatermain32@gmail.com>
2014-09-23 14:01:04 +0200

    Spoločné zadanie 13: Generovanie slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,14 @@
 class Article < ActiveRecord::Base
   belongs_to :category
 
-  validates_uniqueness_of :name
+  before_validation :generate_slug
+  validates_uniqueness_of :name, :slug
+
+  private
+
+  def generate_slug
+    if name_changed? || slug.nil?
+      self.slug = name.parameterize
+    end
+  end
 end
