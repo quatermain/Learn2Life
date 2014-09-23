c8e36005284df2508aa324f5ad92f7053eb008cc
Oliver Kriska <quatermain32@gmail.com>
2014-09-23 13:35:15 +0200

    Spolocne zadanie 9: Zobrazenie clankov a clanku vo formate JSON

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,17 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    respond_to do |format|
+      format.html
+      format.json {render json: @articles}
+    end
   end
 
   def show
     @article = Article.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @article}
+    end
   end
 end
